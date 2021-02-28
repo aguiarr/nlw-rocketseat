06af6d87b4b9619eed0598cce568496727755060
Matheus Aguiar <matheus.aguiar@apiki.com>
2021-02-28 02:24:43 -0300

    Criando e integrando a tabela/classe Surveys

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import {Router} from "express";
+import { SurveysConttoller } from "./controllers/SurveysController";
 import { UserController } from "./controllers/UserController";
 
 const router = Router();
 
 const userController = new UserController();
+const surveysController = new SurveysConttoller();
+
 router.post("/users", userController.create);
+
+router.post("/surveys", surveysController.create)
+router.get("/surveys", surveysController.show)
 
 export { router }; 
